8fce13bf6fad2a31f73f6f8ddd329295d0a0525a
root-mega <65620837+root-mega@users.noreply.github.com>
2024-09-01 18:01:37 +0200

    hi

---
+++ FieldDefaultValue/Il2Cpp.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldDefaultValue
+{
+    public class Il2Cpp
+    {
+        public struct Il2CppGlobalMetadataHeader
+        {
+            public int Sanity { get; set; }
+            public int Version { get; set; }
+            public int StringLiteralOffset { get; set; } // string data for managed code
+            public int StringLiteralCount { get; set; }
+            public int StringLiteralDataOffset { get; set; }
+            public int StringLiteralDataCount { get; set; }
+            public int StringOffset { get; set; } // string data for metadata
+            public int StringCount { get; set; }
+            public int EventsOffset { get; set; } // Il2CppEventDefinition
+            public int EventsCount { get; set; }
+            public int PropertiesOffset { get; set; } // Il2CppPropertyDefinition
+            public int PropertiesCount { get; set; }
+            public int MethodsOffset { get; set; } // Il2CppMethodDefinition
+            public int MethodsCount { get; set; }
+            public int ParameterDefaultValuesOffset { get; set; } // Il2CppParameterDefaultValue
+            public int ParameterDefaultValuesCount { get; set; }
+            public int FieldDefaultValuesOffset { get; set; } // Il2CppFieldDefaultValue
+            public int fieldDefaultValuesCount { get; set; }
+            public int FieldAndParameterDefaultValueDataOffset { get; set; } // uint8_t
+            public int FieldAndParameterDefaultValueDataCount { get; set; }
+            public int FieldMarshaledSizesOffset { get; set; } // Il2CppFieldMarshaledSize
+            public int FieldMarshaledSizesCount { get; set; }
+            public int ParametersOffset { get; set; } // Il2CppParameterDefinition
+            public int ParametersCount { get; set; }
+            public int FieldsOffset { get; set; } // Il2CppFieldDefinition
+            public int FieldsCount { get; set; }
+            public int GenericParametersOffset { get; set; } // Il2CppGenericParameter
+            public int GenericParametersCount { get; set; }
+            public int GenericParameterConstraintsOffset { get; set; } // TypeIndex
+            public int GenericParameterConstraintsCount { get; set; }
+            public int GenericContainersOffset { get; set; } // Il2CppGenericContainer
+            public int GenericContainersCount { get; set; }
+            public int NestedTypesOffset { get; set; } // TypeDefinitionIndex
+            public int NestedTypesCount { get; set; }
+            public int InterfacesOffset { get; set; } // TypeIndex
+            public int InterfacesCount { get; set; }
+            public int VtableMethodsOffset { get; set; } // EncodedMethodIndex
+            public int VtableMethodsCount { get; set; }
+            public int InterfaceOffsetsOffset { get; set; } // Il2CppInterfaceOffsetPair
+            public int InterfaceOffsetsCount { get; set; }
+            public int TypeDefinitionsOffset { get; set; } // Il2CppTypeDefinition
+            public int TypeDefinitionsCount { get; set; }
+            public int RgctxEntriesOffset { get; set; } // Il2CppRGCTXDefinition
+            public int RgctxEntriesCount { get; set; }
+            public int ImagesOffset { get; set; } // Il2CppImageDefinition
+            public int ImagesCount { get; set; }
+            public int AssembliesOffset { get; set; } // Il2CppAssemblyDefinition
+            public int AssembliesCount { get; set; }
+            public int MetadataUsageListsOffset { get; set; } // Il2CppMetadataUsageList
+            public int MetadataUsageListsCount { get; set; }
+            public int MetadataUsagePairsOffset { get; set; } // Il2CppMetadataUsagePair
+            public int MetadataUsagePairsCount { get; set; }
+            public int FieldRefsOffset { get; set; } // Il2CppFieldRef
+            public int FieldRefsCount { get; set; }
+        }
+        public struct Il2CppFieldDefaultValue
+        {
+            public int FieldIndex { get; set; }
+            public int TypeIndex { get; set; }
+            public int DefaultValueDataIndex { get; set; }
+        }
+    }
+}
---
+++ FieldDefaultValue/Program.cs
@@ -0,0 +1,116 @@
+﻿using FieldDefaultValue;
+using System;
+using System.Runtime.InteropServices;
+using static FieldDefaultValue.Il2Cpp;
+
+public class Program
+{
+    static Il2CppGlobalMetadataHeader ReadHeader(string filePath)
+    {
+        Il2CppGlobalMetadataHeader header = new Il2CppGlobalMetadataHeader();
+
+        using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+        using (BinaryReader reader = new BinaryReader(fs))
+        {
+            header.Sanity = reader.ReadInt32();
+            header.Version = reader.ReadInt32();
+            header.StringLiteralOffset = reader.ReadInt32();
+            header.StringLiteralCount = reader.ReadInt32();
+            header.StringLiteralDataOffset = reader.ReadInt32();
+            header.StringLiteralDataCount = reader.ReadInt32();
+            header.StringOffset = reader.ReadInt32();
+            header.StringCount = reader.ReadInt32();
+            header.EventsOffset = reader.ReadInt32();
+            header.EventsCount = reader.ReadInt32();
+            header.PropertiesOffset = reader.ReadInt32();
+            header.PropertiesCount = reader.ReadInt32();
+            header.MethodsOffset = reader.ReadInt32();
+            header.MethodsCount = reader.ReadInt32();
+            header.ParameterDefaultValuesOffset = reader.ReadInt32();
+            header.ParameterDefaultValuesCount = reader.ReadInt32();
+            header.FieldDefaultValuesOffset = reader.ReadInt32();
+            header.fieldDefaultValuesCount = reader.ReadInt32();
+            header.FieldAndParameterDefaultValueDataOffset = reader.ReadInt32();
+            header.FieldAndParameterDefaultValueDataCount = reader.ReadInt32();
+            header.FieldMarshaledSizesOffset = reader.ReadInt32();
+            header.FieldMarshaledSizesCount = reader.ReadInt32();
+            header.ParametersOffset = reader.ReadInt32();
+            header.ParametersCount = reader.ReadInt32();
+            header.FieldsOffset = reader.ReadInt32();
+            header.FieldsCount = reader.ReadInt32();
+            header.GenericParametersOffset = reader.ReadInt32();
+            header.GenericParametersCount = reader.ReadInt32();
+            header.GenericParameterConstraintsOffset = reader.ReadInt32();
+            header.GenericParameterConstraintsCount = reader.ReadInt32();
+            header.GenericContainersOffset = reader.ReadInt32();
+            header.GenericContainersCount = reader.ReadInt32();
+            header.NestedTypesOffset = reader.ReadInt32();
+            header.NestedTypesCount = reader.ReadInt32();
+            header.InterfacesOffset = reader.ReadInt32();
+            header.InterfacesCount = reader.ReadInt32();
+            header.VtableMethodsOffset = reader.ReadInt32();
+            header.VtableMethodsCount = reader.ReadInt32();
+            header.InterfaceOffsetsOffset = reader.ReadInt32();
+            header.InterfaceOffsetsCount = reader.ReadInt32();
+            header.TypeDefinitionsOffset = reader.ReadInt32();
+            header.TypeDefinitionsCount = reader.ReadInt32();
+            header.RgctxEntriesOffset = reader.ReadInt32();
+            header.RgctxEntriesCount = reader.ReadInt32();
+            header.ImagesOffset = reader.ReadInt32();
+            header.ImagesCount = reader.ReadInt32();
+            header.AssembliesOffset = reader.ReadInt32();
+            header.AssembliesCount = reader.ReadInt32();
+            header.MetadataUsageListsOffset = reader.ReadInt32();
+            header.MetadataUsageListsCount = reader.ReadInt32();
+            header.MetadataUsagePairsOffset = reader.ReadInt32();
+            header.MetadataUsagePairsCount = reader.ReadInt32();
+            header.FieldRefsOffset = reader.ReadInt32();
+            header.FieldRefsCount = reader.ReadInt32();
+
+            fs.Close();
+        }
+        return header;
+    }
+
+    static void Main(string[] args)
+    {
+        Il2CppGlobalMetadataHeader header = ReadHeader("./global-metadata.dat");
+
+        int structSize = Marshal.SizeOf<Il2CppFieldDefaultValue>();
+        int metadata_array_field_default_val_size = 0;
+
+        Il2CppFieldDefaultValue[] fieldDefaultValues = new Il2CppFieldDefaultValue[header.fieldDefaultValuesCount];
+
+        Console.WriteLine("Assuming global-metadata.dat is at the application cwd.");
+
+        Console.WriteLine($"FieldDefaultValuesOffset: {header.FieldDefaultValuesOffset}");
+        Console.WriteLine($"header.fieldDefaultValuesCount: {header.fieldDefaultValuesCount}");
+        Console.WriteLine($"Calculating size of FieldDefaultValuesOffset...");
+        metadata_array_field_default_val_size = header.fieldDefaultValuesCount * 12;
+        Console.WriteLine($"Size: {metadata_array_field_default_val_size}");
+
+        using (FileStream fs = new FileStream("global-metadata.dat", FileMode.Open, FileAccess.Read))
+        using (BinaryReader reader = new BinaryReader(fs))
+        {
+            fs.Seek(header.FieldDefaultValuesOffset, SeekOrigin.Begin);
+
+            for (int i = 0; i < header.fieldDefaultValuesCount; i++)
+            {
+                fieldDefaultValues[i] = new Il2CppFieldDefaultValue
+                {
+                    FieldIndex = reader.ReadInt32(),
+                    TypeIndex = reader.ReadInt32(),
+                    DefaultValueDataIndex = reader.ReadInt32()
+                };
+            }
+        }
+
+        Console.WriteLine($"Successfully retrieved {fieldDefaultValues.Length} default values!");
+        Console.WriteLine($"Input your FieldIndex value!");
+        int field_idx_to_search = Convert.ToInt32(Console.ReadLine());
+
+        Il2CppFieldDefaultValue result = Array.Find(fieldDefaultValues, f => f.FieldIndex == field_idx_to_search);
+
+        Console.WriteLine($"Default value is at {result.DefaultValueDataIndex + header.FieldDefaultValuesOffset}");
+    }
+}
